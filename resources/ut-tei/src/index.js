006bcf5b35f0c4b8f1f1467f1c1dde05dce0437b
Mat Jordan <mat@utk.edu>
2018-11-02 10:00:31 -0400

    Bug fix for React invariant error.

--- resources/ut-tei/src/index.js
+++ resources/ut-tei/src/index.js
@@ -17,4 +17,14 @@ import './scss/ut-tei.scss';
 import './vendor/bootstrap/bootstrap-grid.scss';
 import './vendor/fontello/css/fontello.scss';
 
-ReactDOM.render(<Header />, document.getElementById('utk-lib-header'));
+function run() {
+    ReactDOM.render(<Header />, document.getElementById('utk-lib-header'));
+}
+
+const loadedStates = ['complete', 'loaded', 'interactive'];
+
+if (loadedStates.includes(document.readyState) && document.body) {
+    run();
+} else {
+    window.addEventListener('DOMContentLoaded', run, false);
+}
