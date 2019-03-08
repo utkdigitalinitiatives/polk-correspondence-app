8e5fc35cc6ed937e9c5da583fbd07b13264b7474
Mark Baggett <mbagget1@utk.edu>
2019-03-08 11:56:09 -0500

    Try using travis environmental variables.

--- tests/test_build.sh
+++ tests/test_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../
-NAME=$(git branch | grep \* | cut -d ' ' -f2)
+NAME=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 echo "Clone and Checkout Branch ${NAME}"
 cd ../ && git clone https://github.com/utkdigitalinitiatives/utk-docker-existdb.git utk_exist
 cd utk_exist && sed "$d" .env && sed -i "s/BRANCH=master/BRANCH=${NAME}/" .env
