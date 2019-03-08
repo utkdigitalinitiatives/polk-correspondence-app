7d952796c42644e35bd16a23c0b6ecff48fabf75
Mark Baggett <mbagget1@utk.edu>
2019-03-08 11:05:25 -0500

    Add echo so we can figure out why this will not work.

--- tests/test_build.sh
+++ tests/test_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd ../
 NAME=$(git rev-parse --abbrev-ref HEAD)
-cd ../../ && git clone https://github.com/utkdigitalinitiatives/utk-docker-existdb.git utk_exist
+echo "Clone and Checkout Branch ${NAME}"
+cd ../ && git clone https://github.com/utkdigitalinitiatives/utk-docker-existdb.git utk_exist
 cd utk_exist && sed "$d" .env && sed -i "s/BRANCH=master/BRANCH=${NAME}/" .env
 docker-compose up --build -d
