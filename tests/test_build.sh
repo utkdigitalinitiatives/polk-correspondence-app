31d229c979b27f306e5d12dcb967da9e1022a838
Mark Baggett <mbagget1@utk.edu>
2019-03-08 10:03:36 -0500

    Switch variable name for testing.

--- tests/test_build.sh
+++ tests/test_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+NAME=$(git rev-parse --abbrev-ref HEAD)
 cd ../../ && git clone https://github.com/utkdigitalinitiatives/utk-docker-existdb.git utk_exist
-cd utk_exist && sed '$d' .env && sed -i 's/BRANCH=master/BRANCH=${BRANCH}/' .env
+cd utk_exist && sed '$d' .env && sed -i 's/BRANCH=master/BRANCH=${NAME}/' .env
 docker-compose up --build -d
