adf5758398231c620b8c0750409de10ea8d551dc
Mark Baggett <mbagget1@utk.edu>
2019-03-08 09:55:46 -0500

    Switch to in place editing with sed.

--- tests/test_build.sh
+++ tests/test_build.sh
@@ -2,5 +2,5 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 cd ../../ && git clone https://github.com/utkdigitalinitiatives/utk-docker-existdb.git utk_exist
-cd utk_exist && sed '$d' .env && sed -e '\$aBRANCH=${BRANCH}' .env
+cd utk_exist && sed '$d' .env && sed -i 's/BRANCH=master/BRANCH=${BRANCH}/' .env
 docker-compose up --build -d
